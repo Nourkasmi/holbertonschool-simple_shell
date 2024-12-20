700264130c7755d4581ea9673e89a61235c1abc3
NourKasmi <nourkasmi@gmail.com>
2024-12-20 09:53:55 +0100

    execve

--- getline.c
+++ getline.c
@@ -2,19 +2,24 @@
 
 char *get_line(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
 
-	printf("($) ");
-	nread = getline(&line, &len, stdin);
-	if (nread == -1)
-	{
-		free(line);
-		printf("\n");
-		exit(EXIT_SUCCESS);
-	}
-	if (line[nread - 1] == '\n')
-		line[nread - 1] = '\0';
-	return (line);
+    printf("($) ");
+    nread = getline(&line, &len, stdin);
+    if (nread == -1)
+    {
+        free(line);
+        printf("\n");
+        exit(EXIT_SUCCESS);
+    }
+    if (line[nread - 1] == '\n')
+        line[nread - 1] = '\0';
+    if (strcmp(line, "exit") == 0)
+    {
+        free(line);
+        exit(EXIT_SUCCESS);
+    }
+    return (line);
 }
